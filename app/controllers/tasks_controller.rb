654c9d3d0c2a880a3e35e240f512df603de7450c
You9977 <kaounakh0205@yahoo.co.jp>
2021-09-28 22:05:12 +0900

    base-file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,29 @@
 class TasksController < ApplicationController
   def index
-    
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new
+    @task.save
+    redirect_to root_url
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_url
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+task1 = Task.create(title: "test1", category: "jobs", task: "testtask", deadline: Date.today)
+task2 = Task.create(title: "test2", category: "others", task: "testtask2", deadline: Date.today)
